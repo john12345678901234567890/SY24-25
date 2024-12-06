41506867f733b3d6dca132e3d21156cb72e28aa6
John <jdmat690@learner.dvusd.org>
2024-12-06 12:23:40 -0700

    pixelpaint

--- PIXEL PAINT/PIXEL PAINT/MainWindow.xaml.vb
+++ PIXEL PAINT/PIXEL PAINT/MainWindow.xaml.vb
@@ -1,3 +1,9 @@
 ﻿Class MainWindow
-
+    Private Sub fill1_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles fill1.MouseDown, fill2.MouseDown, fill3.MouseDown, fill4.MouseDown, fill5.MouseDown, fill6.MouseDown, fill7.MouseDown, fill8.MouseDown
+        brush1.Fill = sender.Fill
+    End Sub
+    Private Sub p00_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles p00.MouseDown, p01.MouseDown, p02.MouseDown, p03.MouseDown, p04.MouseDown, p05.MouseDown, p06.MouseDown,
+        sender.fill = brush1.Fill
+    End Sub
 End Class
+
